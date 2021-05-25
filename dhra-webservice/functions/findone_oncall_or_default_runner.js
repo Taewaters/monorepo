2dfd764b83534ca25ca441e80cbb2d7ac6074a24
Nicholas Cassiani <ndcassiani@gmail.com>
2021-05-25 09:12:17 -0500

    cleaned up webservices runners should not get calls routed to themselves

--- dhra-webservice/functions/findone_oncall_or_default_runner.js
+++ dhra-webservice/functions/findone_oncall_or_default_runner.js
@@ -1,8 +1,12 @@
 exports = async function(arg) {
+  let query = { $or: [{ onCall: true }, { default: true }]}
+  if (arg && arg.Caller) {
+    query = { $and: [{ number: { $not: { $regex: `/^\\${arg.Caller}/` } }}, query ]}
+  }
   var collection = context.services.get("mongodb-atlas").db("DHRA_PROXY").collection("fieldAgents");
-  var cursor = collection.find({ $or: [{ onCall: true }, { default: true }]}).sort({onCall: -1, lastSession: 1});
+  var cursor = collection.find(query).sort({onCall: -1, lastSession: 1});
   const runners = await cursor.toArray();
-  return runners[0]
+  return runners[0];
   /*
     Accessing application's values:
     var x = context.values.get("value_name");
